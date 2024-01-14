f004cff70daba18525757e64a913a7a30167d1b3
yana392 <yanagino-yosuke719@g.ecc.u-tokyo.ac.jp>
2024-01-14 23:23:20 +0900

    表示の変更

--- static/script.js
+++ static/script.js
@@ -5,3 +5,9 @@ setInterval(() => {
     window.location.reload();
   }
 }, 5 * 1000);
+
+function sayUser(message) {
+  const chatbox ='<li><div class="balloon balloon-r">' + '<p class="say say-r">' + message + '</p>' + '</div></li>';
+  $('#chat-area').append(chatbox);
+  $(window).scrollTop($('#chat-area')[0].scrollHeight);
+  }
